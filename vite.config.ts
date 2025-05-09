cf545532ae37c12bbaef2a3744022e960f943c7e
Aryan Kumar <teamobsidianxd@gmail.com>
2025-05-09 20:11:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/coaching-wallah/',  // Add this line - replace with your repo name
+  base: '/mycw/',  // Add this line - replace with your repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
